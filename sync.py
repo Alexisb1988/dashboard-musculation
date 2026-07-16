6d4c0642057b2aa729310b1246dee53f25e3e4cb
Alexisb1988 <a.baugas@gmail.com>
2026-07-16 14:17:05 +0200

    Update sync.py

--- sync.py
+++ sync.py
@@ -12,5 +12,15 @@ url = "https://api.hevyapp.com/v1/workouts?page=1&pageSize=5"
 
 response = requests.get(url, headers=headers)
 
-print("Status :", response.status_code)
-print(response.text)
+print(f"Status : {response.status_code}")
+
+if response.status_code == 200:
+    data = response.json()
+
+    workouts = data.get("workouts", [])
+    print(f"Nombre de séances reçues : {len(workouts)}")
+
+    for workout in workouts:
+        print(f"- {workout.get('title')} ({workout.get('start_time')})")
+else:
+    print(response.text)
