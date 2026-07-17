c04e6cb70bd3ab37e42597f6d095fea0fa5880fd
Alexisb1988 <a.baugas@gmail.com>
2026-07-17 16:29:24 +0200

    Create utils.py

---
+++ modules/utils.py
@@ -0,0 +1,16 @@
+"""
+Fonctions utilitaires communes au projet.
+"""
+
+
+def log(message):
+    """
+    Affiche un message d'information.
+
+    Paramètres
+    ----------
+    message : str
+        Message à afficher.
+    """
+
+    print(f"[INFO] {message}")
