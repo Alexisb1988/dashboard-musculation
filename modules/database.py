bddab4cda338c08946db026d7742e9291964d0af
Alexisb1988 <a.baugas@gmail.com>
2026-07-17 17:13:40 +0200

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -1,20 +1,19 @@
 """
-Gestion des fichiers JSON du projet.
+Gestion des fichiers générés.
 """
 
+from datetime import datetime
 import json
-from pathlib import Path
 
+
+# ==========================================================
+# Sauvegarde JSON
+# ==========================================================
 
 def save_json(data, filename):
-    """
-    Sauvegarde un objet Python au format JSON.
-    """
-
-    Path("data").mkdir(exist_ok=True)
 
     with open(
-        f"data/{filename}",
+        filename,
         "w",
         encoding="utf-8"
     ) as file:
@@ -22,6 +21,59 @@ def save_json(data, filename):
         json.dump(
             data,
             file,
-            indent=2,
+            indent=4,
             ensure_ascii=False
         )
+
+
+# ==========================================================
+# Métadonnées
+# ==========================================================
+
+def save_metadata(
+    downloaded_count,
+    kept_count,
+    ignored_count,
+    workouts
+):
+    """
+    Génère metadata.json.
+    """
+
+    if len(workouts) == 0:
+
+        first_workout = None
+        last_workout = None
+
+    else:
+
+        dates = sorted(
+            workout["start_time"]
+            for workout in workouts
+        )
+
+        first_workout = dates[0]
+        last_workout = dates[-1]
+
+    metadata = {
+
+        "version": "0.2.3",
+
+        "last_sync": datetime.now().astimezone().isoformat(),
+
+        "downloaded_workouts": downloaded_count,
+
+        "kept_workouts": kept_count,
+
+        "ignored_workouts": ignored_count,
+
+        "first_workout": first_workout,
+
+        "last_workout": last_workout
+
+    }
+
+    save_json(
+        metadata,
+        "metadata.json"
+    )
